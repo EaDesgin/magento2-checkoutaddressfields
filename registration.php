7442a27ec8f9fef0dce99e67e81c1a504f9b1e25
Lucian <office@eadesign.ro>
2017-05-16 20:59:58 +0300

    #1366: Initial module config and composer

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2017 EaDesign by Eco Active S.R.L. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Eadesigndev_Checkoutaddressfields',
+    __DIR__
+);
